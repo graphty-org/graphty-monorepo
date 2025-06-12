75c6239c7390f72b9addda6a4ea001d77d326903
Adam Powers <apowers@ato.ms>
2025-06-11 17:25:18 -0700

    refactor(util): delete obsolete utils

--- graphty-element/index.ts
+++ graphty-element/index.ts
@@ -3,7 +3,6 @@ export {Node} from "./src/Node";
 export {Edge} from "./src/Edge";
 export {Graphty} from "./src/graphty-element";
 export {Styles} from "./src/Styles";
-export * as util from "./src/util";
 export type {GraphOptsType} from "./src/config";
 export {LayoutEngine} from "./src/layout/LayoutEngine";
 export {DataSource} from "./src/data/DataSource";

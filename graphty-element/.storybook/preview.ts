2c8995d4bbfff74501169b9f98c494b8243a6f3a
Adam Powers <apowers@ato.ms>
2025-07-06 15:39:07 -0700

    docs(storybook): add eruda to storybook for mobile debugging

--- graphty-element/.storybook/preview.ts
+++ graphty-element/.storybook/preview.ts
@@ -1,10 +1,13 @@
 // import "@storybook/addon-console";
-
 import {Preview, setCustomElementsManifest} from "@storybook/web-components-vite";
+import eruda from "eruda";
 // @ts-expect-error TS doesn't recognize virtual imports?
 import manifest from "virtual:vite-plugin-cem/custom-elements-manifest";
 
 import DocumentationTemplate from "./DocumentationTemplate.mdx";
+eruda.init();
+eruda.show("console");
+eruda.position({x: window.innerWidth - 60, y: 20});
 
 setCustomElementsManifest(manifest);
 

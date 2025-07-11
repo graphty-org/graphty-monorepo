8f8b542581b18b55ccad4ca6f45de5df0400ba58
Adam Powers <apowers@ato.ms>
2025-07-11 14:53:27 -0700

    style(eslint): update eslint rule to add "@typescript-eslint/no-non-null-assertion": "error"

--- graphty-element/eslint.config.js
+++ graphty-element/eslint.config.js
@@ -43,6 +43,7 @@ export default tseslint.config(
             "@typescript-eslint/no-unsafe-call": "off",
             "@typescript-eslint/restrict-template-expressions": "off",
             "@typescript-eslint/no-unsafe-return": "off",
+            "@typescript-eslint/no-non-null-assertion": "error",
 
             /* *********************
              * BASIC STYLE

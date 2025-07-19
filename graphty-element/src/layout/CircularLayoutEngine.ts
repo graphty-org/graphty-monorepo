51cb18e9219412111a586bf64fd5d812f13d038d
Adam Powers <apowers@ato.ms>
2025-07-19 09:53:48 -0700

    fix: 3d layout for circular

--- graphty-element/src/layout/CircularLayoutEngine.ts
+++ graphty-element/src/layout/CircularLayoutEngine.ts
@@ -6,7 +6,7 @@ import {SimpleLayoutConfig, SimpleLayoutEngine} from "./LayoutEngine";
 export const CircularLayoutConfig = z.strictObject({
     ... SimpleLayoutConfig.shape,
     scale: z.number().positive().default(1),
-    center: z.array(z.number()).length(2).or(z.null()).default(null),
+    center: z.array(z.number()).min(2).max(3).or(z.null()).default(null),
     dim: z.number().default(2),
 });
 export type CircularLayoutConfigType = z.infer<typeof CircularLayoutConfig>;
@@ -14,7 +14,7 @@ export type CircularLayoutOpts = Partial<CircularLayoutConfigType>;
 
 export class CircularLayout extends SimpleLayoutEngine {
     static type = "circular";
-    static maxDimensions = 2;
+    static maxDimensions = 3;
     scalingFactor = 80;
     config: CircularLayoutConfigType;
 

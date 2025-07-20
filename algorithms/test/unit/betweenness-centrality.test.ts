7e930fd9390c7d7dfa4f9d7a3cdeef5888829859
Adam Powers <apowers@ato.ms>
2025-07-20 07:49:18 -0700

    test: disable dot-notation linting for tests

--- algorithms/test/unit/betweenness-centrality.test.ts
+++ algorithms/test/unit/betweenness-centrality.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import {describe, expect, it} from "vitest";
 
 import {betweennessCentrality, edgeBetweennessCentrality, nodeBetweennessCentrality} from "../../src/algorithms/centrality/betweenness.js";

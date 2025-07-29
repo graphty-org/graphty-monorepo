0028d7a3f9a680719a2b505b3409df2cddaf20a4
Adam Powers <apowers@ato.ms>
2025-07-28 18:28:28 -0700

    docs: fix shell layout

--- graphty-element/stories/Layout2D.stories.ts
+++ graphty-element/stories/Layout2D.stories.ts
@@ -115,14 +115,29 @@ export const Shell: Story = {
             graph: {
                 twoD: true,
                 layout: "shell",
+                layoutOptions: {
+                    nlist: [
+                        [0],  // Core - 1 node at center
+                        [1, 2, 3, 4, 5],  // Frameworks - 5 nodes
+                        [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],  // Tools - 15 nodes
+                        [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50],  // Applications - 30 nodes
+                        [51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119]  // Services - 69 nodes
+                    ],
+                    scale: 3,
+                },
             },
         }),
         shellScale: 1,
+        dataSource: "json",
+        dataSourceConfig: {
+            data: "../test/helpers/data5.json",
+        },
     },
     parameters: {
         controls: {
             include: [
                 "graph.layoutOptions.scale",
+                "graph.layoutOptions.nlist",
             ],
         },
     },
